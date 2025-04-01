6752aade76a9e7f93cc291533a0b66b9ee62116a
Hannes Coulembier <hannes.coulembier@gmail.com>
2025-04-02 00:42:56 +0200

    Added sorting

--- static/scripts/compare.js
+++ static/scripts/compare.js
@@ -1,20 +1,41 @@
 let controllerData;
-let filterconfig;
+let filterconfig = {
+    "Reversed":false,
+    "SortBy":"Name"
+}
 function renderTable() {
+    $('#main-table').empty();
+
+    let selection = controllerData.controllers;
+    // Select something
+    selection = _.sortBy(controllerData.controllers, filterconfig["SortBy"]);
+    if (filterconfig["Reversed"]==true) {
+        selection = selection.reverse();
+    }
 
     topRow = document.createElement('tr');
     for (const header of controllerData.layout) {
         const cell = document.createElement('th');
-        cell.innerText = header;
+        cell.innerText = header[Object.keys(header)[0]];
         topRow.appendChild(cell);
     }
     $('#main-table').append(topRow);
 
-    for (const controller of controllerData.controllers) {
-        row = document.createElement('tr');
+    for (const controller of selection) {
+        let row = document.createElement('tr');
         for (const header of controllerData.layout) {
+            let title=Object.keys(header)[0];
             const cell = document.createElement('td');
-            cell.innerText = controller[header];
+            if (title=="Name") {
+                let link = document.createElement('a');
+                link.href = controller["link"];
+                link.innerText = controller[title];
+                link.target = "_blank";
+                cell.appendChild(link);
+            }
+            else {
+                cell.innerText = controller[title];
+            }
             row.appendChild(cell);
         }
         $('#main-table').append(row);
@@ -33,10 +54,28 @@ $(document).ready(()=>{
         .then(data => {
             controllerData = data;
             renderTable();
+
+            for (const header of controllerData.layout) {
+                title=Object.keys(header)[0];
+                opt = document.createElement('option');
+                opt.value=title;
+                opt.innerText=header[title];
+                $('#sortBy').append(opt);
+            }
         })  
         .catch(error => console.error('Failed to fetch data:', error));
+
     $('#filter-button').click(()=>{
         console.log("test");
+    })
+
+    $('#sortBy').change(()=>{
+        filterconfig["SortBy"]=$("#sortBy").children("option:selected").val();
+        renderTable();
+    })
+    $('#order').change(()=>{
+        filterconfig["Reversed"]=$("#order").is(":checked");
+        renderTable();
     })
     
 })
