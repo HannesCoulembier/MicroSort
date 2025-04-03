346bec3b74410c32384144d728c42422938c92ed
Hannes Coulembier <hannes.coulembier@gmail.com>
2025-04-03 10:41:18 +0200

    Added filter option

--- static/scripts/compare.js
+++ static/scripts/compare.js
@@ -7,8 +7,12 @@ function renderTable() {
     $('#main-table').empty();
 
     let selection = controllerData.controllers;
-    // Select something
-    selection = _.sortBy(controllerData.controllers, filterconfig["SortBy"]);
+    for (const header of controllerData.layout) {
+        let title=Object.keys(header)[0];
+        if (title == 'Name'){ continue; }
+        selection = selection.filter((controller)=> controller[title]>= filterconfig['filters'][title]['min'] & controller[title]<= filterconfig['filters'][title]['max'])
+    }
+    selection = _.sortBy(selection, filterconfig["SortBy"]);
     if (filterconfig["Reversed"]==true) {
         selection = selection.reverse();
     }
@@ -53,15 +57,35 @@ $(document).ready(()=>{
         })
         .then(data => {
             controllerData = data;
-            renderTable();
-
+            filterconfig['filters']={};
             for (const header of controllerData.layout) {
-                title=Object.keys(header)[0];
-                opt = document.createElement('option');
+                let title=Object.keys(header)[0];
+                let opt = document.createElement('option');
                 opt.value=title;
                 opt.innerText=header[title];
                 $('#sortBy').append(opt);
+                
+                if (title == 'Name'){ continue; }
+                let entry = document.createElement('div');
+                let label = document.createElement('label');
+                label.innerText = title+":";
+                label.classList.add('input-label');
+                let min = document.createElement('input');
+                min.placeholder = "min";
+                min.id = title+"-min";
+                min.classList.add('input-text');
+                let max = document.createElement('input');
+                max.placeholder = "max";
+                max.id = title+"-max";
+                max.classList.add('input-text');
+                label.appendChild(min);
+                label.appendChild(max);
+                entry.appendChild(label);
+                $('#filter-content').append(entry);
+                filterconfig['filters'][title]={};
             }
+            updateFilters();
+            renderTable();
         })  
         .catch(error => console.error('Failed to fetch data:', error));
 
@@ -75,6 +99,49 @@ $(document).ready(()=>{
     })
     $('#order').click(()=>{
         filterconfig["Reversed"]=!filterconfig["Reversed"];
+        renderTable();
+    })
+
+    $('.dropdown-btn').click(()=>{
+        $('#filter-content').toggleClass('show');
+    })
+
+    function updateFilters() {
+        for (const header of controllerData.layout) {
+            let title=Object.keys(header)[0];
+            if (title == 'Name'){ continue; }
+
+            let values = controllerData.controllers.map((x)=> x[title]);
+            let minimum = Math.min(...values);
+            let maximum = Math.max(...values);
+
+            let min = $('#'+title+'-min').val();
+            let minval = min;
+            if (minval == '') {
+                minval = minimum;
+            }
+            if (isNaN(minval)) {
+                minval = minimum;
+                $('#'+title+'-min').val('');
+            }
+
+            let max = $('#'+title+'-max').val();
+            let maxval = max;
+            if (maxval == '') {
+                maxval = maximum;
+            }
+            if (isNaN(maxval)) {
+                maxval = maximum;
+                $('#'+title+'-max').val('');
+            }
+
+            filterconfig['filters'][title]['min']=parseFloat(minval);
+            filterconfig['filters'][title]['max']=parseFloat(maxval);
+        }
+    }
+
+    $("#filter-content").change(()=>{
+        updateFilters();
         renderTable();
     })
     
